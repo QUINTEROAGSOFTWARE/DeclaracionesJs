1fe3c1aad797abad3ca127c19cddce6a2a4c9d97
andres quintero <bodysystemsforever@gmail.com>
2023-03-31 16:50:07 -0500

    trabajo de bucles estudiar los faltantes hacer los ejecicios y subir al repo

---
+++ bucles.js
@@ -0,0 +1,71 @@
+var suma =0;
+//suma=suma+1;
+//suma=suma+2;
+//suma=suma+3;
+//suma=suma+4;
+//console.log(suma)
+
+//Imaginate si quisieramos escribir 100 o mas lineas de codigo  aqui es donde entra en juego los bucles que  son una forma rapida y censilla de hacer procesos repetitivos.
+//bucleFor
+ //for (var   i= 0;   i< 10;  i++) {
+   //suma=suma+1;
+   // console.log(suma);
+    
+ //}
+ //Variable iteracion
+ //for(var i=0; i<5; i++){
+   //suma=suma+i;
+   //console.log("Variable iteracion: ", i);
+ //}
+ //console.log("Variable suma:" ,suma)
+
+
+ //Operador While
+ while(suma<3){
+   suma=suma +1;
+   console.log(suma); 
+ }
+//Que diferencias hay entre for y while?
+//for es usdo cuando sabemos la cantidad maximaexacta de pasos que queremos ejecutar.
+// esta se situa como segundo parametro entre los ().
+
+
+//encambio el bucle while es usado cuando no sabemos con certeza cuantos pasos necesitaremos hasta finalizar la ejecucion.
+//mientras siga lloviendo voy a estar en mi cama viendo series en este caso no tenemos ninguna certeza cuando dejara de llover debo de esperar que la condicion  que me auto asigne deje de ser cierta  para salir de la cama cuando deje de llover. 
+//extructura y utilidad de los bucles for y while para realizar acciones repetitivas que pueden probocar que nuestro codigo sea extenso y legible estos nos ayudaran a resolver este problema permitiendonos repetir  una cantidad definida de pasos en pocas lineas de codigo.
+
+//Expresión Switch 
+//La expresión Switch evalúa una condición. 
+//Compara su valor con una instancia case, y ejecuta declaraciones asociadas a ese case, así como las declaraciones en los case que siguen.
+//switch(expr){
+ // case 'Naranjas':
+   // console.log('El kilogramo de naranjas cuesta $0.59');
+  //  break;
+   // case 'Manzanas':
+   // console.log('El kilogramo de manzanas cuesta $0.32');
+   // break;
+   // case 'Platanos':
+   // console.log('El kilogramo de platanos cuesta $0.48');
+    //break;
+   // case 'Cerezas':
+   // console.log('El kilogramo de Cerezas cuesta $3.00');
+    //break;
+    //case 'Mangos':
+    //case 'Papayas':
+    //console.log('El kilogramo de mangos y papayas cuesta $2.79');
+   // break;
+ //  default:
+    //  console.log('Lo lamentamos, por el momento no dispinemos de '+expr +'.');
+   //}
+    //console.log("Hay algo mas que  te quisiera  consultar?");
+//Bucle Do-While
+//El bucle Do-While (hacer mientras) ejecuta una sentencia especificada, hasta que la condición de comprobación se evalúa como falsa.
+//La condición se evalúa después de ejecutar la sentencia, dando como resultado que la sentencia especificada se ejecute al menos una vez.
+
+//Declaración Continue
+//La declaración continue se utiliza dentro de los Bucles For. 
+//Nos permite omitir alguna de las iteraciones si se cumple una condición específica.
+
+//Break
+//La declaración break se utiliza dentro de los Bucles For. 
+//Nos permite "romper" o finalizar el bucle con antelación si se cumple una condición específica.
