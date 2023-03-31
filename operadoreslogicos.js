f7e6f62e2a876f27cc5918e32c4dcb843816fb3a
andres quintero <bodysystemsforever@gmail.com>
2023-03-31 00:15:24 -0500

    modificacion-de-operadores-logicos31mar

--- operadoreslogicos.js
+++ operadoreslogicos.js
@@ -32,6 +32,32 @@ function negacion(permiso){
 negacion(true);
 
 function negacion(permiso){
-    if(!permiso) console.log("Tiene permiso2");
+    if(permiso!== console.log(false)) console.log("Tiene permiso2");
 };
 negacion(false);
+
+
+//Funcion condicioncompleja
+function condicionCompleja(num){
+    if (num > 9 && num %2 ===0 || num === 3) console.log(true);
+    else console.log(false);
+};
+condicionCompleja(10);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(5);
+
+//¿Alguna vez escuchaste el término veracidad? Como el nombre lo indica, refiere a la verdad de algo. Es decir, si es verdadero o falso.
+//su forma de procesar lógica se basa en un sistema binario. Unos y ceros. Verdaderos y falsos. 
+//Es por esto que absolutamente todos los datos dentro de este lenguaje pueden ser resumidos a estos dos valores. 
+
+//Utiliza la función Boolean() e ingresa cualquiera de los siguientes valores para comprobar cuál es su veracidad:
+1 //ture
+0 //false
+-1 //true
+true //true
+false //false
+"string"//true
+null //false
+undefined//undefined
+//[]true
